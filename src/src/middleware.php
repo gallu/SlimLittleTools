50c44efeb40657052abd4693215f42df1aa029e5
gallu <m.furusho@gmail.com>
2018-07-25 23:35:41 +0900

    コメントの修正

--- src/src/middleware.php
+++ src/src/middleware.php
@@ -5,14 +5,13 @@ use SlimLittleTools\Middleware\Cookie;
 use SlimLittleTools\Middleware\CsrfGuard;
 use SlimLittleTools\Middleware\SlimLittleToolsUse;
 
-
-//
+// CSRFの例外リストを引数で渡す
 $CSRF_not_covered_list = [
 ];
 $app->add((new CsrfGuard())->setNotCoveredList($CSRF_not_covered_list));
 
-
-// 以下、特にSlimLittleToolsUseは「一番外側(最後)」にaddすること
+// 定番の諸々
+// XXX SlimLittleToolsUseは「一番外側(最後)」にaddすること
 $app->add(new AddHeader($app->getContainer()));
 $app->add(new Cookie($app->getContainer()));
 $app->add(new SlimLittleToolsUse($app->getContainer()));
